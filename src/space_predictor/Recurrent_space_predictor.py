5a782b34da20219499fe21680e07a763973284bf
harel moshayof <harel.moshayof@intel.com>
2023-05-23 15:51:53 +0300

    rec

--- src/space_predictor/Recurrent_space_predictor.py
+++ src/space_predictor/Recurrent_space_predictor.py
@@ -4,7 +4,7 @@ from src.space_predictor.space_predictor import space_predictor
 from src.space_predictor.Iterative_space_predictor import Iterative_space_predictor
 
 class Recurrent_space_predictor(space_predictor):
-    def genText(self,modelResult:ModelResult,text:str,threshold=0.1)->(str,Model):
+    def genText(self,text:str,threshold=0.1)->str:
         """
         In this function we will predict spaces in the text untill
         :param threshold: certain threshold of certainty
@@ -13,9 +13,8 @@ class Recurrent_space_predictor(space_predictor):
         :return text with certain spaces
         """
 
-        new_txt,new_modelResult= Iterative_space_predictor().genText(modelResult,text,threshold)
-
+        new_txt= Iterative_space_predictor().genText(text,threshold)
         if text==new_txt:
-            return new_txt,new_modelResult
+            return new_txt
         else:
-            return self.genText(new_modelResult,new_txt,threshold)
+            return self.genText(new_txt,threshold)
