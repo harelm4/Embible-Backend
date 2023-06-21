13b14a26849f5f4ff99be95d687e34a67bcac174
Harel , Moshayof <harel.moshayof@intel.com>
2023-06-21 19:43:01 +0300

    include space predictor

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import config
 from huggingface_hub import login
 
 from src.model.ensemble_v2 import EnsembleV2
+from src.space_predictor.Iterative_space_predictor import Iterative_space_predictor
 
 app = Flask(__name__)
 CORS(app)
 login(config.configs['hf_token'])
-ens=EnsembleV2()
+ens=EnsembleV2(Iterative_space_predictor())
 
 @app.route("/calc",methods=['GET'])
 def calc():
